7f87fbe6952fea359d6b4844af1fa204a2deb13c
cs-star <61611390+cs-star@users.noreply.github.com>
2021-03-20 11:48:54 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -9,6 +9,5 @@
 # 软件包地址：https://github.com/281677160/openwrt-package
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone https://github.com/linkease/ddnsto-openwrt package/luci-app-ddnsto
 # git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 # git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
